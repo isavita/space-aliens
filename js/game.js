6824f65ec8caa894c224c9623e840c99dd7e4dbd
Aleksandar <isavitaisa@gmail.com>
2024-03-15 15:08:07 +0000

    Add headers and icons for the page

--- js/game.js
+++ js/game.js
@@ -1,11 +1,23 @@
 /* global Phaser */
 
+// Game scene
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // Set background color
   backgroundColor: 0x5f6e7a,
-  
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 const game = new Phaser.Game(config);
